8ea7bf829d22037add8e4f658a94eceaefb65421
umaidali236 <umaid236@gmail.com>
2024-05-11 13:37:50 +0530

    updated files

--- src/script.js
+++ src/script.js
@@ -1,28 +1,22 @@
-const count = document.querySelector(".count");
-const buttons = document.querySelector(".buttons");
-
-buttons.addEventListener("click", (e) => {
-    console.log(e.target.classList);
-
-    if (e.target.classList.contains("add")) {
-        count.innerHTML++;
-    }
-    if (e.target.classList.contains("subtract")) {
-        count.innerHTML--;
-    }
-    if (e.target.classList.contains("reset")) {
-        count.innerHTML = 0;
-    }
-
-    setColor();
+const container = document.querySelector(".container");
+const joinBtn = document.querySelector(".navigation a:nth-child(3)");
+const homeBtn = document.querySelector(".navigation a:nth-child(2)");
+const loginBtn = document.querySelector(".contact p a");
+const loginMsg = document.querySelector(".contact p span");
+const loginHeader = document.querySelector(".contact h1");
+const contactBtn = document.querySelector(".contact-btn:nth-child(2)");
+const contactForm = document.querySelector(".contact-form");
+joinBtn.addEventListener("click", () => {
+    container.classList.add("change");
 });
-
-function setColor() {
-    if (count.innerHTML > 0) {
-        count.style.color = "greenyellow";
-    } else if (count.innerHTML < 0) {
-        count.style.color = "#fe9f7d";
-    } else {
-        count.style.color = "#fff";
-    }
-}
+homeBtn.addEventListener("click", () => {
+    container.classList.remove("change");
+});
+loginBtn.addEventListener("click", () => {
+    contactForm.reset();
+    container.classList.toggle("hide")
+        ? (loginBtn.textContent = "Sign Up") && (contactBtn.textContent = "Log In") &&
+        (loginMsg.textContent = "Don't have an account?") && (loginHeader.textContent = "Welcome Back")
+        : (loginBtn.textContent = "Log In") && (contactBtn.textContent = "Create Account") &&
+        (loginMsg.textContent = "Already have an account?") && (loginHeader.textContent = "Create New Account");
+});
